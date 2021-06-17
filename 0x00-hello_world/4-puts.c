c3464ab854ddd1ec8f54738606e94d0dd9d45183
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-17 17:54:16 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
- #include <stdio.h>
+ #include  <stdio.h>
+
 /**
-* main - entry for  c coding challenge
-* in low level coding section.
-* Return: 0 returns zero
-*/
+ *main - This uses the puts function to print line to standard output
+ *
+ * Return: 0
+ */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
