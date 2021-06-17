8b541c3d49669166405103579b2964a6cd2762b2
kjnat <my24playstation@gmail.com>
2021-06-17 11:13:04 -0700

    SIze

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Function to run code
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+  printf("Size of a char: %lu byte(s)\n", sizeof(char));
+  printf("Size of an int: %lu byte(s)\n", sizeof(int));
+  printf("Size of a short: %lu byte(s)\n", sizeof(short));
+  printf("Size of a long: %lu byte(S)\n", sizeof(long));
+}
