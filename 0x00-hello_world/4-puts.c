86cae8d1b3e1066f4ca0b4184c4566b64559b94d
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-17 17:43:05 +0000

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
 /**
- *main - Entry point
- *
- *Return:Always 0 (Success)
- */
+* main - entry for  c coding challenge
+* in low level coding section.
+* Return: 0 returns zero
+*/
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+	/**
+	 * This code puts the below line.
+	*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
