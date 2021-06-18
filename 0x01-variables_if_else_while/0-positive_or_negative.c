cab2a8106c8802c8c3ce97e745457bea4c43fee1
kjnat <my24playstation@gmail.com>
2021-06-18 05:33:24 -0700

    Positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+
+/* more header goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n > 0)
+  {
+     return ("is positive.\n");
+  }
+  else if (n = 0)
+  {
+      return ("is zero.\n");
+  }
+  else
+  {
+      return ("is negative.\n");
+  }
+  return (0);
+}
