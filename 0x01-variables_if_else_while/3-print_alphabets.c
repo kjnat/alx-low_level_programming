7f2db887440b29ba526707b4875f6c93fa5fd6ee
kjnat <my24playstation@gmail.com>
2021-06-18 11:32:12 -0700

    Both upper and lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
  */
 int main()
 {
-        char i;
+         char i;
 
-        for (i = 'a'; i <= 'z'; i++)
-        {
+         for (i = 'a'; i <= 'z'; i++)
+         {
              putchar(i);
-        }
-        for (i = 'A'; i <= 'Z'; i++)
-        { 
+         }
+         for (i = 'A'; i <= 'Z'; i++)
+         { 
              putchar(i);
-        }
-        putchar('\n');
+         }
+         putchar('\n');
 return (0);
 }
