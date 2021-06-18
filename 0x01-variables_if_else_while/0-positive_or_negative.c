6232a5ccc38e020bcd841a197c0ae338481f9e7c
kjnat <my24playstation@gmail.com>
2021-06-18 07:26:42 -0700

    Positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
   n = rand() - RAND_MAX / 2;
 
   if ( n > 0 ) {
-     printf("is positive\n" );
+    printf("is positive\n" );
   } else if ( n == 0 ) {
-      printf("is zero\n" );
+    printf("is zero\n" );
   } else {
-      printf("is negative\n" );
+    printf("is negative\n" );
   }
    return (0);
 }
