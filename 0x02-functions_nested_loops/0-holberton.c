255f20c5a325b440315a27a1f12b7bc8822a68eb
kjnat <my24playstation@gmail.com>
2021-06-22 05:52:57 -0700

    Print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,6 +6,13 @@
  */
 int main(void)
 {
-	write(1, "Holberton\n", 10)
+	int str[ ] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sb;
+	sb = sizeof(str) / sizeof(int);
+	for (count = 0; count < sb; sb++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');	
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+int _putchar(char);
+
+
+
+
+#endif
