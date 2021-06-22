ea353a307efb2ccedcc7fd23a57d38a5130ed01b
kjnat <my24playstation@gmail.com>
2021-06-22 06:04:24 -0700

    Print Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	int str[ ] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
 	int count, sb;
 	sb = sizeof(str) / sizeof(int);
 	for (count = 0; count < sb; sb++)
 	{
 		_putchar(str[count]);
 	}
-	_putchar('\n');	
+	_putchar('\n');
 	return (0);
 }
