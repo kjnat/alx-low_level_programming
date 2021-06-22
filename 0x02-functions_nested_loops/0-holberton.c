83d42199d5e6a9c8dd596190ebf0a35b762e01ea
kjnat <my24playstation@gmail.com>
2021-06-22 05:12:42 -0700

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * main - prints Holberton as a message
+ * Description - print Holberton
+ * Return: Always 0
+ */
+int main(void)
+{
+	write(1, "Holberton\n", 10)
+	return (0);
+}
