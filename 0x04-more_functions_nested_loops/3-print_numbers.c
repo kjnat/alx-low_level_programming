0ea11490fa8fe2f7339e7c16831812dfddfe3fa2
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-24 17:30:17 +0000

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,18 @@
+#include "holberton.h"
 
+/**
+ * print_numbers - Print numbers from 0 to 9.
+ * @c: Print c to stdout
+ * Return: 0
+ */
+void print_numbers(void)
+{
+  int c;
+  
+	for (c = 48; c <= 57; c++)
+  {
+    _putchar(c);
+  }
+  _putchar('\n');
+  Return (0);
+}
