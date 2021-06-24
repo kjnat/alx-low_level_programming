5c7f7bedd868a850d55e0c9ac9d01b7302c0ebd4
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-24 16:14:52 +0000

    Create 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Entry point
+ * Description: checks is a character is uppercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+int _upper(int c)
+{
+int i = 'A';
+
+for (i = 'A'; i <= 'Z'; i++)
+{
+if (c == i)
+{
+return (1);
+}
+else if
+{
+  return (0);
+}
+}
+}
