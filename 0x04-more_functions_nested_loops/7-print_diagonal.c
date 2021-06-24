2e5f386cbb828bb60075f5a916ea73b6a78f0d97
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-24 19:30:53 +0000

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,17 @@
+#include "holberton.h"
 
+/**
+ * print_diagonal - prints straight line n times.
+ * @n: times diagonal line is printed.
+ * Return: no return.
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+	}
+	_putchar('\n');
+}
