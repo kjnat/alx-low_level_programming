e124f81880d6c99a890926545216aef96ae8cb39
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-24 11:56:26 +0000

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -30,10 +30,6 @@ printf("Remaining days: %d\n", 365 - day);
 }
 else if (year % 100 == 0)
 {
-if (month >= 2 && day >= 60)
-{
-day++;
-}
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
