9eff908e1c09a3717900ba91f2f7eb464dea9473
kjnat <my24playstation@gmail.com>
2021-06-24 10:43:52 -0700

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 
 /**
  * print_numbers - Print numbers from 0 to 9.
- * @c: Print c to stdout
- * Return: 0
+ * @c: Print c
+ * Return: no return
  */
 void print_numbers(void)
 {
-  int c;
-  
-	for (c = 48; c <= 57; c++)
-  {
-    _putchar(c);
-  }
-  _putchar('\n');
-  Return (0);
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
