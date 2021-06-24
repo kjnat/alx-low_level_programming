d396b4380555cf9995c8cd1e579ceb943d367a03
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-24 18:48:05 +0000

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_most_numbers - prints numbers between 0 to 9 except 2 and 4.
+ * print_most_numbers - prints numbers between 0 to 9 
+ * (except 2 and 4)
  * Return: no return.
  */
 void print_most_numbers(void)
