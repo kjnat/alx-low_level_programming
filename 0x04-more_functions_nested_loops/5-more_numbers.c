143405b566ed0fe146d115689a4b40c90c75fafc
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-24 19:01:51 +0000

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,23 @@
 
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 10x numbers from 0 to 14.
+ * Return: no return.
+ */
+void more_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 14; ch++)
+	{ 
+    int a = 0;
+    while (a < 9)
+    {
+      _putchar(a);
+      a++;
+      }
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
