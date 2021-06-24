996e6893d4ca8a17c119904c07d5d558742bbfca
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-24 18:30:18 +0000

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints numbers between 0 to 9 except 2 and 4.
+ * Return: no return.
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
