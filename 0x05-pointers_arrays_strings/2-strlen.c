8a5bb98686ec1074f221e06a7035424983a955ed
kjnat <my24playstation@gmail.com>
2021-06-28 05:31:10 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,15 @@
 #include "holberton.h"
+#include <string.h>
 /**
  * _strlen - Function that returns the length of a string
  * @s: string character
- * Return: no return
+ * Return: len of a string
  */
 int _strlen(char *s)
 {
-	strlen(s);
+	int len = 0;
+
+	while (*(s + len != '\0')
+			len++;
+	Return (len);
 }
