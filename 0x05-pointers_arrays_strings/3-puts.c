489f677661afd9ff3041d26384d2d787d6dbac58
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-28 13:29:20 +0000

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,22 @@
-#include "holberton.h"                                                                                                                  
-/**                                                                                                                     
- * _puts - Function that prints a string                                                                                                  
- * @str: input character                                                                                                   
- * Return: string                                                                                                                   
- */                                                                                                                        
-void _puts(char *str)                                                                                                                      
-{                                                                                                                      
-        int count = 0;
+#include "holberton.h"
 
-        while (count >= 0)                                                                                                                 
-        {                                                                                                                 
-                if (str[count] == '\0')                                                                                                    
-                {                                                                                                    
-                        _putchar('\n');                                                                                                    
-                        break;                                                                                                    
-                }                                                                                                             
-                _putchar(str[count]);                                                                                                      
-                count++;                                                                                                      
-        }                                                                                                                   
-}  
+/**
+ * _puts - prints a string, followed by a new line.
+ * @str: input string.
+ * Return: no return.
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
