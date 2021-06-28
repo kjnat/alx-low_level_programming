8a953378885d10a6e3f63102ca9b674fefbe4fcb
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-28 13:17:59 +0000

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,7 +6,16 @@
  */                                                                                                                                        
 void _puts(char *str)                                                                                                                      
 {                                                                                                                                          
-        char *str;                                                                                                                          
-        _puts("*str \n");                                                                                                                   
-                                                                                                                                           
+        int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}                                                                                                                
 }     
