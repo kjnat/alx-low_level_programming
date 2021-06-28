e65eda7e0356c773317fdfe0cf62f6a561b5df2d
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-28 13:14:19 +0000

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "holberton.h"                                                                                                                     
+/**                                                                                                                                        
+ * _puts - Function that prints a string                                                                                                   
+ * @str: input character                                                                                                                   
+ * Return: string                                                                                                                          
+ */                                                                                                                                        
+void _puts(char *str)                                                                                                                      
+{                                                                                                                                          
+        char str;                                                                                                                          
+        _puts("str \n");                                                                                                                   
+                                                                                                                                           
+}     
