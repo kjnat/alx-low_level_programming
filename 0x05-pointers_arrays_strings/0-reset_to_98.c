2f400bc211e3acb29a269104a7a6aa26d03b9233
kjnat <my24playstation@gmail.com>
2021-06-28 04:29:16 -0700

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,14 +2,9 @@
 /**
  * reset_to_98 - function that takes a pointer to an int parameter
  * @n - variable pointer to int parameter
- * Return: 0
+ * Return: no return
  */
 void reset_to_98(int *n)
 {
-int var = 98;
-int *n;
-
-n = &var;
-putchar(n);
-Return (0);
+	*n = 98;
 }
