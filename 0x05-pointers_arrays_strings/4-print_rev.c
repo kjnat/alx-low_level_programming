d8b4da01446f13b368470b18cf99f76a0c5f692f
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-28 14:12:42 +0000

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"                                                                                                                     
+                                                                                                                                           
+/**                                                                                                                                        
+ * print_rev - prints a string in reverse, followed by a new line.                                                                         
+ * @s: input string.                                                                                                                       
+ * Return: no return.                                                                                                                      
+ */                                                                                                                                        
+void print_rev(char *s)                                                                                                                    
+{                                                                                                                                          
+        int count = 0;                                                                                                                     
+                                                                                                                                           
+        while (count >= 0)                                                                                                                 
+        {                                                                                                                                  
+                if (s[count] == '\0')                                                                                                      
+                {                                                                                                                          
+                        _putchar('\n');                                                                                                    
+                        break;                                                                                                             
+                }                                                                                                                          
+                print_rev(s[count]);                                                                                                       
+                count++;                                                                                                                   
+        }                                                                                                                                  
+}           
