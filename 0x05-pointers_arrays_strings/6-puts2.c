21e268645b4b1474ad2d72fa2f60b21d94e35977
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-28 17:44:33 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,13 @@ void puts2(char *str)
 
 	while (count >= 0)
 	{
-		if(str[count] == '\0')
+		if (str[count] == '\0')
 		{
 			_putchar('\n');
 			break;
 		}
-	}
-  
+		if (count % 2 == 0)
+			_putchar(str[count]);
+		count++;
+	}  
 }
