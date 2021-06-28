2a37b8d736298e4cd3885812daac6091c2da3c44
kjnat <46638938+kjnat@users.noreply.github.com>
2021-06-28 13:28:26 +0000

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,21 @@
-#include "holberton.h"                                                                                                                     
-/**                                                                                                                                        
- * _puts - Function that prints a string                                                                                                   
- * @str: input character                                                                                                                   
- * Return: string                                                                                                                          
- */                                                                                                                                        
+#include "holberton.h"                                                                                                                  
+/**                                                                                                                     
+ * _puts - Function that prints a string                                                                                                  
+ * @str: input character                                                                                                   
+ * Return: string                                                                                                                   
+ */                                                                                                                        
 void _puts(char *str)                                                                                                                      
-{                                                                                                                                          
-        int count = 0;                                                                                                                     
-                                                                                                                                           
+{                                                                                                                      
+        int count = 0;
+
         while (count >= 0)                                                                                                                 
-        {                                                                                                                                  
+        {                                                                                                                 
                 if (str[count] == '\0')                                                                                                    
-                {                                                                                                                          
+                {                                                                                                    
                         _putchar('\n');                                                                                                    
-                        break;                                                                                                             
-                }                                                                                                                          
+                        break;                                                                                                    
+                }                                                                                                             
                 _putchar(str[count]);                                                                                                      
-                count++;                                                                                                                   
-        }                                                                                                                                  
-}          
+                count++;                                                                                                      
+        }                                                                                                                   
+}  
