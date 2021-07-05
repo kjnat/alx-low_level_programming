42294c51a068b63980a91bc95430da8bcd5672b6
kjnat <my24playstation@gmail.com>
2021-07-05 05:14:45 -0700

    0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _memset - function that fills memory with constant byte
+ * @s: pointer to memory area to be filled
+ * @b: character to fill memory area with
+ * @n: number of bytes to be filled
+ * Return: pointer to filled memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	_memset(s, 'b', sizeof(n));
+}
