9e4657427a469a3aaf38de58c41c85230706b751
kjnat <my24playstation@gmail.com>
2021-07-05 08:49:23 -0700

    3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: string
+ * @accept: character
+ * Return: number of bytes in s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+
+}
