40470ca0540ec275dcf9ce853247c0dd315ee9a6
kjnat <46638938+kjnat@users.noreply.github.com>
2021-07-09 15:09:50 +0000

    Create _strlen.c

---
+++ 0x09-static_libraries/_strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _strlen - Function to print length of a string
+ * @s: input
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
