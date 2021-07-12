8ad2de274359dc45bd8f5c4aad914b113805746b
kjnat <my24playstation@gmail.com>
2021-07-12 10:48:56 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers
+ * @argc: number of command line argument
+ * @argv: array that contains command line argument
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+
+	return (0);
+}
