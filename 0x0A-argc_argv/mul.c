ce58536933d03327cedec3f7a31b475a13d0aad6
kjnat <46638938+kjnat@users.noreply.github.com>
2021-07-12 17:33:26 +0000

    Create mul.c

---
+++ 0x0A-argc_argv/mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplies two integers.
+ * @a: First integer
+ * @b: Second integer
+ * Return: Multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
