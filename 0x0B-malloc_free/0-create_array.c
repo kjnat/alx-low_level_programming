3d3bdc96dae8f57824bc84798909d9d81fadbff0
kjnat <my24playstation@gmail.com>
2021-07-13 07:40:04 -0700

    0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: characters
+ * Return: pointer of an array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(c) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+	return (array);
+}
