1a065741f1e26abea19a08b1003f81724a7a36b6
kjnat <46638938+kjnat@users.noreply.github.com>
2021-07-15 14:32:26 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,30 +7,34 @@
  * @n: number of bytes                                                
  * @Return: a pointer to the newly allocated space                    
  */                                                                   
-char *string_concat(char *s1, char *s2, unsigned int n)               
-{                                                                     
-        char *concat_str;                                             
-        int i;                                                        
-        len = 0;                                                      
-        concat_index = 0;                                             
-                                                                      
-        if (s1 == NULL)                                               
-                s1 = " ";                                             
-        if (s2 == NULL)                                               
-                s2 = " ";                                             
-        for (i = 0; i < len; i++)                                     
-                len++;                                                
-        concat_str = malloc(sizeof(s1) + ((sizeof(s2) * n)));         
-                                                                      
-        if (concat_str == NULL)                                       
-                return (NULL);                                        
-        if (n >= s2[i])                                               
-                return (s2[i] + 1);                                   
-                                                                      
-        for (i = 0; s1[i]; i++)                                       
-                concat_str[concat_index] = s1[i];                     
-        for (i = 0; s2[i]; i++)                                       
-                concat_str[concat_index] = s2[i];                     
-                                                                      
-        return (concat_str);                                          
-} 
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	unsigned int len = n, index;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
+}
