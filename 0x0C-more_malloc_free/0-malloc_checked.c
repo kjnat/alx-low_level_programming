2124b6f4328d45993fe4f6626efe558b1a0ab395
kjnat <46638938+kjnat@users.noreply.github.com>
2021-07-15 15:38:29 +0000

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,7 +10,7 @@ void *malloc_checked(unsigned int b)
 {
 	char *allocated_memory;
 
-	allocated_memory = malloc(sizeof(b));
+	allocated_memory = malloc(b);
 	if (allocated_memory == NULL)
 		exit(98);
 	return (allocated_memory);
