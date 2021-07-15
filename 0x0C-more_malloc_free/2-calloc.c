7f415292b2f17f98c0237eb68d0d79f9966990a1
kjnat <46638938+kjnat@users.noreply.github.com>
2021-07-15 18:35:56 +0000

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,19 +11,19 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *m;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	m = malloc(nmemb * size);
+	p = malloc(nmemb * size);
 
-	if (m == NULL)
+	if (p == NULL)
 		return (NULL);
 
 	for (i = 0; i < (nmemb * size); i++)
-		m[i] = 0;
+		p[i] = 0;
 
-	return (m);
+	return (p);
 }
