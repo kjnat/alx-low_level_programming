ec18a0c2bbc9a010e549b3fd9ccefd05f58b3102
kjnat <46638938+kjnat@users.noreply.github.com>
2021-07-21 18:11:46 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - new description of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
