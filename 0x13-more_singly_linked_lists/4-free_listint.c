d90f60547172816f141e3ecaaee83e501370215a
kjnat <my24playstation@gmail.com>
2021-08-03 07:29:06 -0700

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a list
+ * @h: pointer to the address of head of the list
+ * Return: no return
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
