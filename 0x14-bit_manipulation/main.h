0e0871c40bee6c76b0564bab1e4228e08a646f08
kjnat <46638938+kjnat@users.noreply.github.com>
2021-08-05 13:35:28 +0000

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,12 @@
-#ifndef _HOLBERTON_H
-#define _HOLBERTON_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x13-bit_manipulation directory.
+ */
+
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
@@ -8,6 +14,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-int _putchar(char c);
 
-#endif
+#endif /* HOLBERTON_H */
